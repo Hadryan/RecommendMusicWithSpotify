539e8c327797b6932172762bceda1a10b4d34337
KentaMatsumura <photosynthesishope@gmail.com>
2021-06-05 22:15:41 +0900

    データバインディングの実装

--- app/src/main/java/jp/matsumura/kenta/recommendmusicwithspotify/MainActivity.kt
+++ app/src/main/java/jp/matsumura/kenta/recommendmusicwithspotify/MainActivity.kt
@@ -2,10 +2,14 @@ package jp.matsumura.kenta.recommendmusicwithspotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import jp.matsumura.kenta.recommendmusicwithspotify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
